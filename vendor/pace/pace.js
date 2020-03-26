65c11e7e9b063926192991fc309ddb225e4fdd9b
Bing Lu <3262633+binglu-sh@users.noreply.github.com>
2020-03-26 08:57:17 +0800

    fixed multiple 'pace-running' class issue
    when start a pace task, the component may add multiple 'pace-running' class into the body.

--- vendor/pace/pace.js
+++ vendor/pace/pace.js
@@ -275,7 +275,9 @@ Bar = (function() {
       this.el = document.createElement('div');
       this.el.className = "pace pace-active";
       document.body.className = document.body.className.replace(/pace-done/g, '');
-      document.body.className += ' pace-running';
+      if (!new RegExp(/pace-running/).test(document.body.className)){
+        document.body.className += ' pace-running';
+      }
       this.el.innerHTML = '<div class="pace-progress">\n  <div class="pace-progress-inner"></div>\n</div>\n<div class="pace-activity"></div>';
       if (targetElement.firstChild != null) {
         targetElement.insertBefore(this.el, targetElement.firstChild);
